711a1f32f7d20306d555767889b4bdd472844016
Sebastian Cursaro <scursaro@cpp.edu>
2022-10-21 17:46:48 -0700

    guess game template

---
+++ GuessGame.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    //We will need to make constant ints for the min possible number and max.
+    const int MIN = 1;
+    const int MAX = 100;
+
+    // we will also need ints for the guess, and the answer. I want to add how may guesses
+    int guess;
+    int guesses;
+    int answer;
+
+    //we will need to have an output that asks for options
+    // 1: play the game
+    //2: change max number
+    //3: to quit 
+    // maybe a switch case nested in a while loop
+
+    //Option 1 selected, runs through the game
+
+    //once guess found, restarts the program
+
+    //once guess is q, restart program, not end
+
+
+    //Option 2, not sure what this means tbh. maybe max is 100
+    //no negative numbers
+
+    //Option 3, Thanks for playing ends program
+
+
+
+
+
+
+    return 0;
+}
