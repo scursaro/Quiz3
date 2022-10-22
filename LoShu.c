4e3552cd15f8334a59170af49bba8c5b2f273ef4
Sebastian Cursaro <scursaro@cpp.edu>
2022-10-21 21:07:34 -0700

    Created isBoardMagic() function

--- LoShu.c
+++ LoShu.c
@@ -20,11 +20,31 @@ int isBoardMagic(int arr[3][3])
     int magic = -1; //negative one so if not found it will be negative 1
 
     //for loop to iterate through columns
-
-
+    for(int i =0; i < 3; i++)
+    {
+        int oneSum = 0;
+        for(int j =0; j < 3; j++)
+        {
+            oneSum += arr[i][j];
+        }
+        if(oneSum != 15) magic =0;
+    }
     //for loop to iterate through rows
+    for(int j =0; j < 3; j++)
+    {
+        int twoSum = 0;
+        for(int i =0; i< 3; i++)
+        {
+            twoSum += arr[i][j];
+        }
+        if(twoSum != 15) magic =0;
+    }
 
     //finally iterate through diagonals
+    if(arr[0][0] + arr[1][1] + arr[2][2] != 15) magic =0;
+    if(arr[2][0] + arr[1][1] + arr[0][2] != 15) magic =0;
+
+    return magic;
 }
 
 //There should be a function that prints the array
